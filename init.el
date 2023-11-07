886c4f4cee46770b446af769aad0ec4c75ddbdeb
Nidish Balaji <nbalaji@ila.uni-stuttgart.de>
2023-11-08 00:43:26 +0100

    local capture

--- init.el
+++ init.el
@@ -7,6 +7,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(org-agenda-files nil)
  '(package-selected-packages
    '(csv-mode org-tree-slide multiple-cursors realgud yasnippet-capf yasnippet-snippets escalator org-roam dired-subtree ov org-bullets org-special-block-extras lsp-ui counsel-projectile projectile company-box python-mode matlab-mode cart quelpa-use-package pdf-tools magit iedit helm-swoop helm-ls-git crdt company-quickhelp company-auctex auctex-latexmk)))
 (custom-set-faces
