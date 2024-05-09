5037ddecbcaf94261ab5b8e807f9b6c0d54b1e6b
Nidish Narayanaa Balaji <nidbid@gmail.com>
2024-05-09 21:56:18 +0200

    Update elaunch.sh so emacs launches in reasonable size

--- elaunch.sh
+++ elaunch.sh
@@ -2,7 +2,8 @@
 
 source ~/.profile
 
-emacsclient -ce '(lambda () (interactive) previous-buffer)'
+# emacsclient -ce '(lambda () (interactive) previous-buffer)'
+emacsclient -ce '(set-frame-size (selected-frame) 106 41)'
 if [ $? -ne 0 ]; then
     emacs --daemon
     emacsclient -c
