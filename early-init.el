761e610eafce7a45168247d334c5482ac8dcf5c9
Nidish <nidish@iitm.ac.in>
2026-05-15 16:26:04 +0530

    Vertico over Helm!

---
+++ early-init.el
@@ -0,0 +1,8 @@
+;; (setq package-enable-at-startup nil
+;;       package-quickstart nil)
+(setq package-enable-at-startup nil
+      package-quickstart nil
+      package-check-signature nil
+      package-native-compile nil
+      package-menu-hide-low-priority t
+      package-menu-lazy-check nil)
